25bd57993eec73004b4dba8aa33859531215c9cf
sebastianpantinliljevall <seblilj@gmail.com>
2018-07-11 13:46:35 +0200

    removed override for RN higher than 0.45

--- android/src/main/java/com/wog/videoplayer/VideoPlayerPackage.java
+++ android/src/main/java/com/wog/videoplayer/VideoPlayerPackage.java
@@ -20,6 +20,10 @@ public class VideoPlayerPackage implements ReactPackage {
         return Arrays.<NativeModule>asList(new VideoPlayerModule(reactContext));
     }
 
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
+    }
+
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
